396b02d6c2a7b27bf89a86cddfbf833da60bc1ba
LauroEmm <lauro.rafael.ismart@gmail.com>
2023-11-23 21:02:56 -0300

    feat: add initial board

--- src/Controllers/IAController.js
+++ src/Controllers/IAController.js
@@ -1,13 +1,57 @@
 const IAModel = require("../Models/IAModel");
 
 class IAController {
-  async create(req, res) {}
+  async create(req, res) {
+    try {
+      const ferramenta = await IAModel.create(req.body);
 
-  async read(req, res) {}
+      return res.status(200).json(ferramenta);
+    } catch (error) {
+      res.status(500).json({ message: "Deu ruim aqui", error: error.message });
+    }
+  }
 
-  async destroy(req, res) {}
+  async read(req, res) {
+    try {
+      const ferramenta = await IAModel.find();
 
-  async update(req, res) {}
+      return res.status(200).json(ferramenta);
+    } catch (error) {
+      res.status(500).json({ message: "Deu ruim aqui", error: error.message });
+    }
+  }
+
+  async destroy(req, res) {
+    try {
+      const { id } = req.params;
+
+      const ferramentaEncontrada = await IAModel.findById(id);
+
+      if (!ferramentaEncontrada)
+        return res.status(404).json({ message: "Ferramenta não foi encontrada" });
+
+      await ferramentaEncontrada.deleteOne();
+      return res.status(200).json({ mensagem: "Ferramenta deletada com sucesso!" });
+    } catch (error) {
+      res.status(500).json({ message: "Deu ruim aqui", error: error.message });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      const ferramentaEncontrada = await IAModel.findById(id);
+
+      if (!ferramentaEncontrada)
+        return res.status(404).json({ message: "Ferramenta não foi encontrada" });
+
+      await ferramentaEncontrada.set(req.body).save();
+      return res.status(200).json({ mensagem: "Ferramenta mudada com sucesso!" });
+    } catch (error) {
+      res.status(500).json({ message: "Deu ruim aqui", error: error.message });
+    }
+  }
 }
 
 module.exports = new IAController();
--- src/Validators/IAValidator.js
+++ src/Validators/IAValidator.js
@@ -2,11 +2,30 @@ const { z } = require("zod");
 const { validateRequest } = require("zod-express-middleware");
 const { default: mongoose } = require("mongoose");
 
-const create = validateRequest({});
+const create = validateRequest({
+  body: z.object({
+    nome: z.string({ required_error: "O nome é obrigatório" }),
+    urlImagem: z.string({ require_error: "A url da imagem é obrigatória" }),
+    descricao: z.string({ require_error: "A descrição é obrigatória" }),
+  }),
+});
 
-const destroy = validateRequest({});
+const destroy = validateRequest({
+  params: z.object({
+    id: z.custom(mongoose.isValidObjectId, "O Id não é válido"),
+  }),
+});
 
-const update = validateRequest({});
+const update = validateRequest({
+  body: z.object({
+    nome: z.string({ required_error: "O nome é obrigatório" }),
+    urlImagem: z.string({ require_error: "A url da imagem é obrigatória" }),
+    descricao: z.string({ require_error: "A descrição é obrigatória" }),
+  }),
+  params: z.object({
+    id: z.custom(mongoose.isValidObjectId, "O Id não é válido"),
+  }),
+});
 
 module.exports = {
   create,
