d23613c917a38613f5ec20237428fb07f89f0f3d
Lucas Velloso <lucasabreuvelloso@gmail.com>
2023-11-26 12:06:45 -0300

    controler e validator
    ainda n testei

--- src/Controllers/IAController.js
+++ src/Controllers/IAController.js
@@ -1,13 +1,58 @@
 const IAModel = require("../Models/IAModel");
 
 class IAController {
-  async create(req, res) {}
+  async create(req, res) {
+    try{
+    const IA = await IAModel.create(req.body);
+    
+    return res.status(200).json(IA);
+    } catch (error) {
+      res.status(500).json({message: "Deu ruim aqui!!", error: error.message})
+    }
+  }
 
-  async read(req, res) {}
+  async read(req, res) {
+    try{
+    const IA = await IAModel.find();
 
-  async destroy(req, res) {}
+    return res.status(200).json(IA);
+    } catch(error){
+      res.status(500).json({message: "Deu ruim aqui!!", error: error.message})
+    }
+  }
 
-  async update(req, res) {}
+  async destroy(req, res) {
+    try{
+    const {id} = req.params;
+    const ID_encontrado = await IAModel.findById(id);
+
+    if(!ID_encontrado)
+    return res.status(404).json({message: "Ferramenta não encontrada"})
+
+    await IAModel.findByIdAndDelete(id);
+
+    return res.status(200).json({ "mensagem": "Ferramenta deletada com sucesso"});
+    } catch(error){
+      res.status(500).json({message: "Deu ruim aqui!!", error: error.message})
+    }
+  }
+
+  async update(req, res) {
+    try{
+    const {id} = req.params;
+    const ID_encontrado = await IAModel.findById(id);
+
+    if(!ID_encontrado)
+      return res.status(404).json({message: "Ferramenta não encontrada"});   
+
+    const IA = await IAModel.findByIdAndUpdate(id, req.body, {new: true});
+
+    return res.status(200).json(IA);
+    } catch(error){
+      res.status(500).json({message: "Deu ruim aqui!!", error: error.message})
+    }
+
+  }
 }
 
 module.exports = new IAController();
--- src/Models/IAModel.js
+++ src/Models/IAModel.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const IASchema = new mongoose.Schema({});
+const IASchema = new mongoose.Schema({
+ nome: String,
+ link: String,
+ descricao: String
+});
 
 const IAModel = mongoose.model("IA", IASchema);
 module.exports = IAModel;
