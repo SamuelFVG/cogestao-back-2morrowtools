b41948e3eee1bad0a7e364ebc6f3c40a63512f84
Lucas Velloso <lucasvelloso@cpejr.com.br>
2023-11-26 20:14:29 -0300

    mudando a rota
    /ferramentas

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,9 @@ const IAValidator = require("./Validators/IAValidator");
 
 const routes = Router();
 
-routes.post("/IA", IAValidator.create, IAController.create);
-routes.get("/IA/", IAController.read);
-routes.delete("/IA/:id", IAValidator.destroy, IAController.destroy);
-routes.put("/IA/:id", IAValidator.update, IAController.update);
+routes.post("/ferramentas", IAValidator.create, IAController.create);
+routes.get("/ferramentar/", IAController.read);
+routes.delete("/ferramentas/:id", IAValidator.destroy, IAController.destroy);
+routes.put("/ferramentas/:id", IAValidator.update, IAController.update);
 
 module.exports = routes;
