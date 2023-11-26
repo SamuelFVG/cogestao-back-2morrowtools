914e1bd759f1d374c661689e0c932e58a398a966
Lucas Velloso <lucasvelloso@cpejr.com.br>
2023-11-26 20:08:27 -0300

    back finalizado
    testei

--- src/Validators/IAValidator.js
+++ src/Validators/IAValidator.js
@@ -3,29 +3,28 @@ const { validateRequest } = require("zod-express-middleware");
 const { default: mongoose } = require("mongoose");
 
 const create = validateRequest({
-body: z.object({
-    nome: z.string({ required_error: "O nome é obrigatório"}),
-    link: z.string({ required_error: "O link é obrigatório"}).url("O link é inválido"),
-    descricao: z.string({ required_error: "A descrição é obrigatória"}),
- 
-}),
+  body: z.object({
+    nome: z.string({ required_error: "O nome é obrigatório" }),
+    link: z.string({ required_error: "O link é obrigatório" }).url("O link é inválido"),
+    descricao: z.string({ required_error: "A descrição é obrigatória" }),
+  }),
 });
 
 const destroy = validateRequest({
   params: z.object({
-    id: z.custom(mongoose.isValidObjectId, "O id não é válido")
+    id: z.custom(mongoose.isValidObjectId, "O id não é válido"),
   }),
 });
 
 const update = validateRequest({
   body: z.object({
     nome: z.string().optional(),
-    link: z.string().url("O link é inválido").optional(),
+    link: z.string().optional(),
     descricao: z.string().optional(),
-}),
+  }),
   params: z.object({
     id: z.custom(mongoose.isValidObjectId, "O id não é válido"),
-  })
+  }),
 });
 
 module.exports = {
