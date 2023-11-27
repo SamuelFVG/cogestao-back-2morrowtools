364b054f05b635c8df6ef73b4b43a0a3740c952d
Lucas Velloso <lucasabreuvelloso@gmail.com>
2023-11-26 22:01:57 -0300

    Erro no rotas
    arrumado

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const IAValidator = require("./Validators/IAValidator");
 const routes = Router();
 
 routes.post("/ferramentas", IAValidator.create, IAController.create);
-routes.get("/ferramentar/", IAController.read);
+routes.get("/ferramentas/", IAController.read);
 routes.delete("/ferramentas/:id", IAValidator.destroy, IAController.destroy);
 routes.put("/ferramentas/:id", IAValidator.update, IAController.update);
 
