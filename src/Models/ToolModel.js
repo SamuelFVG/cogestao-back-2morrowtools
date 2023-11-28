6d928d987fdbe026dc246adc88411337fa6a7d79
g-franklinn <gfranklinn@hotmail.com>
2023-11-28 14:26:03 -0300

    set versionKey to false

--- src/Models/ToolModel.js
+++ src/Models/ToolModel.js
@@ -6,7 +6,12 @@ const toolSchema = new mongoose.Schema({
     nome: String,
     linkImagem: String,
     descricao: String
-});
+    
+},
+    {
+        versionKey: false
+    }
+);
 
 const toolModel = mongoose.model("tools", toolSchema);
 module.exports = toolModel;
