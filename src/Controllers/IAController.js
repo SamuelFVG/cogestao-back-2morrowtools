8edcb9039a7d75f2c1c285899c97421f0540d389
Mateus Bretz <mateusbretz@cpejr.com.br>
2023-11-29 15:50:15 -0300

    Models e funções Controllers

--- src/Controllers/IAController.js
+++ src/Controllers/IAController.js
@@ -1,13 +1,57 @@
 const IAModel = require("../Models/IAModel");
 
 class IAController {
-  async create(req, res) {}
+  async create(req, res) {
+    try {
+      const ferramenta = await IAModel.create(req.body);
 
-  async read(req, res) {}
+      const { descrição, ...novaFerramenta } = usuario.toObject();
 
-  async destroy(req, res) {}
+      return res.status(200).json(novaFerramenta);
+    } catch (error) {
+      res.status(500).json({ message: "Deu ruim aqui!", error: error.message });
+    }
+  }
 
-  async update(req, res) {}
+  async read(req, res) {
+    try {
+      const ferramentas = await IAModel.find();
+      return res.status(200).json(ferramentas);
+    } catch (error) {
+      res.status(500).json({ message: "Deu ruim aqui!", error: error.message });
+    }
+  }
+
+  async destroy(req, res) {
+    try {
+      const { id } = req.params;
+      const ferramentaEncontrada = await IAModel.findById(id);
+      if (!ferramentaEncontrada) {
+        return res.status(404).json({ message: "Ferramenta não encontrada" });
+      }
+      await ferramentaEncontrada.deleteOne();
+      return res.status(200).json({ mensagem: "Ferramenta deletada com sucesso!" });
+    } catch (error) {
+      res.status(500).json({ message: "Deu ruim aqui!", error: error.message });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      const ferramentaEncontrada = await IAModel.findById(id);
+
+      if (!ferramentaEncontrada)
+        return res.status(404).json({ message: "Ferramenta não encontrada" });
+
+      const ferramenta = await ferramentaEncontrada.set(req.body).save();
+
+      return res.status(200).json(ferramentaEncontrada);
+    } catch (error) {
+      res.status(500).json({ message: "Deu ruim aqui!", error: error.message });
+    }
+  }
 }
 
 module.exports = new IAController();
--- src/Models/IAModel.js
+++ src/Models/IAModel.js
@@ -2,7 +2,20 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const IASchema = new mongoose.Schema({});
+const IASchema = new mongoose.Schema({
+  nome: {
+    type: String,
+    unique: true,
+  },
+  imagem: {
+    type: String,
+    unique: true,
+  },
+  descrição: {
+    type: String,
+    unique: true,
+  },
+});
 
-const IAModel = mongoose.model("IA", IASchema);
+const IAModel = mongoose.model("IAtool", IASchema);
 module.exports = IAModel;
