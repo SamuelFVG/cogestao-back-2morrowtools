d7e3704263c7e64cc22c71d97a04b9466cdc5cb3
Mateus Bretz <mateusbretz@cpejr.com.br>
2023-11-30 13:17:28 -0300

    Validar na pasta Validators

--- src/Validators/IAValidator.js
+++ src/Validators/IAValidator.js
@@ -2,11 +2,32 @@ const { z } = require("zod");
 const { validateRequest } = require("zod-express-middleware");
 const { default: mongoose } = require("mongoose");
 
-const create = validateRequest({});
+const create = validateRequest({
+  body: z.object({
+    nome: z.string({ required_error: "O nome é obrigatório" }),
+    imagem: z
+      .string({ required_error: "A Url da imagem é obrigatória" })
+      .url("A imagem é inválida"),
+    descrição: z.string({ required_error: "A descrição é obrigatória" }),
+  }),
+});
 
-const destroy = validateRequest({});
+const destroy = validateRequest({
+  params: z.object({
+    id: z.custom(mongoose.isValidObjectId, "O id não é válido"),
+  }),
+});
 
-const update = validateRequest({});
+const update = validateRequest({
+  body: z.object({
+    nome: z.string().optional(),
+    imagem: z.string().url("A Url é inválida").optional(),
+    descrição: z.string().optional(),
+  }),
+  params: z.object({
+    id: z.custom(mongoose.isValidObjectId, "O id não é válido"),
+  }),
+});
 
 module.exports = {
   create,
